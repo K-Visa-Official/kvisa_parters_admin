c7ff934f6b22da6afa45ff571eae268c2af850d0
tech@k-visa.co.kr <tech@k-vias.co.kr>
2025-03-06 17:42:43 +0900

    ds

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,55 @@ const nextConfig: NextConfig = {
     domains: ['kvisa-partners.s3.amazonaws.com', 'kvisa-partners.s3.ap-northeast-2.amazonaws.com'],
     
   },
+  async redirects() {
+    return [
+      {
+        source: '/en',
+        destination: '/ko', // 한국어로 리디렉션
+        permanent: false,
+      },
+      {
+        source: '/vi',
+        destination: '/ko',
+        permanent: false,
+      },
+      {
+        source: '/th',
+        destination: '/ko',
+        permanent: false,
+      },
+      {
+        source: '/si',
+        destination: '/ko',
+        permanent: false,
+      },
+      {
+        source: '/ne',
+        destination: '/ko',
+        permanent: false,
+      },
+      {
+        source: '/my',
+        destination: '/ko',
+        permanent: false,
+      },
+      {
+        source: '/mn',
+        destination: '/ko',
+        permanent: false,
+      },
+      {
+        source: '/km',
+        destination: '/ko',
+        permanent: false,
+      },
+      {
+        source: '/id',
+        destination: '/ko',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
